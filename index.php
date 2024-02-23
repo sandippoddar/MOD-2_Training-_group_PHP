da0040f96ae26858d5ce67a938d7f6c488c65b6e
sandippoddar <sandip.poddar@innoraft.com>
2024-02-23 17:57:58 +0530

    FT2-40: Added changes in Task 2.

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
 <?php
-    $full_name="";
-    if (isset($_SERVER["REQUEST_METHOD"])=="POST") {
-        if (isset($_POST['submit']))
-        {
-            $first_name = htmlspecialchars($_POST['first_name']);
-            $last_name = $_POST['last_name'];
-            $full_name = $first_name . ' ' . $last_name;
-        }
-    }
-?>
-<?php
 
     $targetFile= "";
     if (isset($_POST["submit"])) {
@@ -20,8 +9,22 @@
         $tmp_name = $_FILES['image']['tmp_name'];
         $fileext = explode('.', $targetFile);
         $file_type = strtolower($fileext[1]);
-        move_uploaded_file($tmp_name,$targetFile);
+        if ($file_type == "jpg" || $file_type == "jpeg" || $file_type == "png") {
+            move_uploaded_file($tmp_name,$targetFile);
+        }
+        
 
+    }
+?>
+<?php
+    $full_name="";
+    if (isset($_SERVER["REQUEST_METHOD"])=="POST") {
+        if (isset($_POST['submit']))
+        {
+            $first_name = htmlspecialchars($_POST['first_name']);
+            $last_name = $_POST['last_name'];
+            $full_name = $first_name . ' ' . $last_name;
+        }
     }
 ?>
 <!DOCTYPE html>
@@ -33,6 +36,7 @@
 <body>
     <div class="container">
         <form method = "post" action = "index.php" enctype="multipart/form-data">
+            <h1>PHP BASIC</h1>
             <div class = "form-ele">
                 <label for = "first_name">First Name:</label>
                 <input type = "text" name = "first_name" id = "first_name" pattern="[A-Za-z]+" required>
@@ -51,7 +55,7 @@
                 <label for = "full_name"> Full Name: </label>
                 <input type = "text" name = "full_name" id = "full_name" value = "" disabled>
             </div>
-            <div class = "form-ele" class = "btn">
+            <div class = "form-ele btn">
                 <input type = "submit" name = "submit" value="Submit">
             </div>
 
