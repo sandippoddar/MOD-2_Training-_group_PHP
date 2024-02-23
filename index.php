d626e788718f493faf0c37e7ab43b8ad57542c13
sandippoddar <sandip.poddar@innoraft.com>
2024-02-23 16:56:10 +0530

    FT2-40: Added Task 2 in FT2-40-Task2.

--- index.php
+++ index.php
@@ -1,62 +1,77 @@
+<?php
+    $full_name="";
+    if (isset($_SERVER["REQUEST_METHOD"])=="POST") {
+        if (isset($_POST['submit']))
+        {
+            $first_name = htmlspecialchars($_POST['first_name']);
+            $last_name = $_POST['last_name'];
+            $full_name = $first_name . ' ' . $last_name;
+        }
+    }
+?>
+<?php
+
+    $targetFile= "";
+    if (isset($_POST["submit"])) {
+
+        $file = $_FILES['image'];
+        $targetDir="img/";
+        $targetFile = $targetDir . basename($_FILES["image"]["name"]);
+        $tmp_name = $_FILES['image']['tmp_name'];
+        $fileext = explode('.', $targetFile);
+        $file_type = strtolower($fileext[1]);
+        move_uploaded_file($tmp_name,$targetFile);
+
+    }
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Form Example</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <div class="container">
+        <form method = "post" action = "index.php" enctype="multipart/form-data">
+            <div class = "form-ele">
+                <label for = "first_name">First Name:</label>
+                <input type = "text" name = "first_name" id = "first_name" pattern="[A-Za-z]+" required>
+                <p class="wrong_fname"></p>
+            </div>
+            <div class = "form-ele">
+                <label for = "last_name">Last name:</label>
+                <input type = "text" name = "last_name" id = "last_name" pattern="[A-Za-z]+" required>
+                <p class="wrong_lname"></p>
+            </div>
+            <div class="form-ele">
+                <label for="image">Enter Image</label>
+                <input type="file" name="image" id="image">
+            </div>
+            <div class = "form-ele">
+                <label for = "full_name"> Full Name: </label>
+                <input type = "text" name = "full_name" id = "full_name" value = "" disabled>
+            </div>
+            <div class = "form-ele" class = "btn">
+                <input type = "submit" name = "submit" value="Submit">
+            </div>
 
-    <form method = "post" action = "index.php" enctype="multipart/form-data">
-        <div class = "form-ele">
-            <label for = "first_name">First_Name:</label>
-            <input type = "text" name = "first_name" id = "first_name" required>
+        </form>
+        <div class="image">
+            <img src="<?php 
+            
+                 if (isset($_POST['submit'])) {
+                    echo $targetFile;
+                 }
+            ?>" alt="">
         </div>
-        <div class = "form-ele">
-            <label for = "last_name">Last name:</label>
-            <input type = "text" name = "last_name" id = "last_name" required>
-        </div>
-        <div class = "form-ele">
-            <label for = "full_name">Full Name:</label>
-            <input type = "text" name = "full_name" id = "full_name" value = "" disabled>
-        </div>
-        <div class="form-ele">
-            <label for="image">User Image</label>
-            <input type="file" name="image" id="image">
-        </div>
-        <input type = "submit" name = "submit" value = "Submit">
-    </form>
-    <div class="image">
-        <?php
-            if (isset($_POST["submit"])) {
-
-                $file=$_FILES['image'];
-                // echo var_dump($file);
-                print_r($file);
-                
-                $file_name = $_FILES['image']['name'];
-                $tmp_name = $_FILES['image']['tmp_name'];
-                echo $tmp_name;
-                $fileext=explode('.',$file_name);
-                print_r($fileext);
-                $file_type=strtolower($fileext[1]);
-                echo $file_type;
-                $file_new=uniqid('',true).".".$file_type;
-                echo $file_new;
-                $file_dest='img/'.$file_new;
-                move_uploaded_file($tmp_name,$file_dest);
-                echo "<img src='$file_dest'/>";
-            }
-        ?>
+        <h1> 
+            <?php 
+                if (isset($_POST['submit'])) {
+                    echo "Hello " . $full_name; 
+                }
+            ?> 
+        </h1>
     </div>
-    <?php
-        if (isset($_POST['submit']))
-        {
-            $first_name = $_POST['first_name'];
-            $last_name = $_POST['last_name'];
-            $full_name = $first_name . ' ' . $last_name;
-            echo "Hello " . $full_name;
-        }
-    ?>
-    </h1>
-    <script src = "./script.js"></script>
+<script src = "script.js"></script>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,9 +1,51 @@
+// storing HTML elements 
 const first_name = document.querySelector("#first_name");
 const last_name = document.querySelector("#last_name");
 let full_name = document.querySelector("#full_name");
-function update ()
-{
-    full_name.value = first_name.value+" "+last_name.value;
+full_name.value = "";
+let regex = /^[a-zA-Z]+$/;
+first_name.addEventListener('input',()=> {
+    if (first_name.value === "") {
+        document.querySelector(".wrong_fname").textContent = "";
+    }
+    else if (!regex.test(first_name.value)) {
+        document.querySelector(".wrong_fname").textContent = "Enter Only Alphabets";
+    }
+    else {
+        document.querySelector(".wrong_fname").textContent = "";
+    }
+})
+last_name.addEventListener('input',()=> {
+    if (last_name.value === ""){
+        document.querySelector(".wrong_lname").textContent = "";
+    }
+    else if (!regex.test(last_name.value)) {
+        document.querySelector(".wrong_lname").textContent="Enter Only Alphabets";
+    }
+    else {
+        document.querySelector(".wrong_lname").textContent = "";
+    }
+})
+// function for update the full_name field
+function update_value() {
+    const first_name1 = first_name.value.replace(/[^a-zA-Z]/g, '');
+    const last_name1 = last_name.value.replace(/[^a-zA-Z]/g, '');
+    if (regex.test(first_name1)) {
+        if (regex.test(last_name1)) {
+            full_name.value = first_name1 + " " + last_name1;
+        }
+        else {
+            full_name.value = first_name1 + " ";
+        }
+    }
+    else {
+        if (regex.test(last_name1)) {
+            full_name.value = " " + last_name1;
+        }
+        else {
+            full_name.value = "";
+        }
+    }
 }
-first_name.addEventListener('input', update);
-last_name.addEventListener('input', update);
+first_name.addEventListener('input', update_value);
+last_name.addEventListener('input', update_value);
