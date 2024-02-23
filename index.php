3ecd8c05941172f56ae4d42dc91b8f577ce90629
sandippoddar <sandip.poddar@innoraft.com>
2024-02-23 14:17:30 +0530

    FT2-40: Added changes as PR review in Task 1.

--- index.php
+++ index.php
@@ -1,36 +1,50 @@
+<?php
+    $full_name="";
+    if (isset($_SERVER["REQUEST_METHOD"])=="POST") {
+        if (isset($_POST['submit']))
+        {
+            $first_name = htmlspecialchars($_POST['first_name']);
+            $last_name = $_POST['last_name'];
+            $full_name = $first_name . ' ' . $last_name;
+        }
+    }
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Form Example</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
-<form method = "post" action = "index.php">
-    <div class = "form-ele">
-        <label for = "first_name">First_Name:</label>
-        <input type = "text" name = "first_name" id = "first_name" required>
+    <div class="container">
+        <form method = "post" action = "index.php">
+            <div class = "form-ele">
+                <label for = "first_name">First Name:</label>
+                <input type = "text" name = "first_name" id = "first_name" pattern="[A-Za-z]+" required>
+                <p class="wrong_fname"></p>
+            </div>
+            <div class = "form-ele">
+                <label for = "last_name">Last name:</label>
+                <input type = "text" name = "last_name" id = "last_name" pattern="[A-Za-z]+" required>
+                <p class="wrong_lname"></p>
+            </div>
+            <div class = "form-ele">
+                <label for = "full_name"> Full Name: </label>
+                <input type = "text" name = "full_name" id = "full_name" value = "" disabled>
+            </div>
+            <div class = "form-ele" class = "btn">
+                <input type = "submit" name = "submit" value="Submit">
+            </div>
+            
+        </form>
+        <h1> 
+            <?php 
+                if (isset($_POST['submit'])) {
+                    echo "Hello " . $full_name; 
+                }
+            ?> 
+        </h1>
     </div>
-    <div class = "form-ele">
-        <label for = "last_name">Last name:</label>
-        <input type = "text" name = "last_name" id = "last_name" required>
-    </div>
-    <div class = "form-ele">
-        <label for = "full_name">Full Name:</label>
-        <input type = "text" name = "full_name" id = "full_name" value = "" disabled>
-    </div>
-    <input type = "submit" name = "submit" value="Submit">
-</form>
-<h1>
-<?php
-    if (isset($_POST['submit']))
-    {
-        $first_name = $_POST['first_name'];
-        $last_name = $_POST['last_name'];
-        $full_name = $first_name . ' ' . $last_name;
-        echo "Hello " . $full_name;
-    }
-?>
-</h1>
-<script src = "./script.js"></script>
+<script src = "script.js"></script>
 </body>
 </html>
