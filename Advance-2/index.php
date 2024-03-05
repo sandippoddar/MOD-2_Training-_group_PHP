e067bb7a22de970e5f2366fb7206455bb3efd1b7
sandippoddar <sandip.poddar@innoraft.com>
2024-03-05 18:14:21 +0530

    FT2-43: Added .gitignore in Task2.

--- Advance-2/index.php
+++ Advance-2/index.php
@@ -1,15 +1,12 @@
 <?php
 
     use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\SMTP;
-    use PHPMailer\PHPMailer\Exception;
 
     require 'vendor/autoload.php';
 
     if(isset($_POST['submit'])) {
         $email = $_POST['email'];
-        $mail = new PHPMailer(true);
-        $mail -> SMTPDebug = 1;                           
+        $mail = new PHPMailer(true);                      
         $mail -> isSMTP();                          
         $mail -> Host = 'smtp.gmail.com';
         $mail -> SMTPAuth = true;                      
@@ -22,35 +19,37 @@
         $mail -> isHTML(true);
         $mail -> Subject = 'Submission';
         $mail -> Body = 'Thank you for the Submission.';
-        if(!$mail -> send())
-        {
-            echo "Mailer Error: " . $mail->ErrorInfo;
-        }
-        else
-        {
-            echo "Message has been sent successfully";
-        }
     }
     
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset = "UTF-8">
+    <meta name = "viewport" content="width=device-width, initial-scale=1.0">
     <title>Advance Task2</title>
-    <link rel="stylesheet" href="./CSS/style.css">
+    <link rel = "stylesheet" href = "./CSS/style.css">
 </head>
 <body>
     <div class="container">
-        <form action="index.php" method="post">
+        <form action = "index.php" method = "post">
             <h1>PHP Advance Task2</h1>
             <div class="form-ele">
-                <label for="email">Enter email:</label>
-                <input type="email" name="email" id="email">
+                <label for = "email">Enter email:</label>
+                <input type = "email" name = "email" id = "email">
             </div>
-            <input type="submit" value="Submit" name="submit">
+            <input type = "submit" value = "Submit" name = "submit">
         </form>
+        <h1>
+            <?php
+                if (isset($_POST["submit"])) {
+                    if ($mail -> send())
+                    {
+                        echo "Message has been sent successfully";
+                    }
+                }
+            ?>
+        </h1>
     </div>
 </body>
 </html>
