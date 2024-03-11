f5e0633046312262dad55fd335ec19f85dbdeebf
sandippoddar <sandip.poddar@innoraft.com>
2024-03-11 16:27:48 +0530

    FT2-43: Added  Global variable in Task2.

--- Advance-2/SendEmail.php
+++ Advance-2/SendEmail.php
@@ -2,6 +2,7 @@
 
   use PHPMailer\PHPMailer\PHPMailer;
   use GuzzleHttp\Client;
+  use Dotenv\Dotenv as Dotenv;
   require 'vendor/autoload.php';
 
   /**
@@ -24,15 +25,42 @@
      */
     
     private $mail;
+
+    /**
+     * 
+     * Constructor of class SendEmail().
+     * 
+     * Initialize the instance variable when
+     * the object of class is created.
+     * 
+     */
+
     public function __construct() {
+      $dotenv = Dotenv::createImmutable(__DIR__);
+      $dotenv->load();
       $this->mail = new PHPMailer(TRUE);
     }
+
+    /**
+     * 
+     * Function configureMail($email).
+     * 
+     * This function is use here to return TRUE if The Email
+     * is send or FALSE if the email is not sent.
+     * 
+     * @param string.
+     * Stores Email address of Person to send Email.
+     * 
+     * @return bool.
+     * 
+     */
+
     public function configureMail($email) {
       $this->mail->isSMTP();
       $this->mail->Host = 'smtp.gmail.com';
       $this->mail->SMTPAuth = TRUE;
-      $this->mail->Username = 'sandip.poddar@innoraft.com';
-      $this->mail->Password = 'pbhcctzzenyutbsj';
+      $this->mail->Username = $_ENV['userName'];
+      $this->mail->Password = $_ENV['password'];
       $this->mail->SMTPSecure = 'ssl';
       $this->mail->Port = 465;
       $this->mail->setFrom('sandip.poddar@innoraft.com');
@@ -46,9 +74,24 @@
       return FALSE;
     }
 
+    /**
+     * 
+     * Function validateEmail($email).
+     * 
+     * This function is use here to check the email is
+     * valid or not and if valid now check if this email
+     * exist or not.
+     * 
+     * @param string.
+     *  Stores Email address of Person to send Email.
+     * 
+     * @return bool.
+     * 
+     */
+
     public function validateEmail($email) {
       if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $apiKey = '1e78c4c89237488db4366aee49b9a7e7';
+        $apiKey = $_ENV['apiKey'];
         $client = new Client();
         $response = $client->request('GET', "https://emailvalidation.abstractapi.com/v1?api_key=$apiKey&email=$email");
         $data = json_decode($response->getBody(), TRUE);
